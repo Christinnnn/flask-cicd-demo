a82efac60202e3aa2742ec7c093d801fc3bc337f
Christinnnn <harutyunyanchristin@gmail.com>
2025-06-13 21:27:24 +0400

    new

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # Այս տեքստը պետք է ճիշտ նույնը լինի, ինչ թեստի մեջ
+  
     return "My Awesome SI/SD Pipeline!"
 
 if __name__ == "__main__":
