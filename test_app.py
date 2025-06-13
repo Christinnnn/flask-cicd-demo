ee28b3d0dd3d386bf9122f3d3eda57a881a773fb
Christin <76056536+Christinnnn@users.noreply.github.com>
2025-06-13 18:21:26 +0400

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app as flask_app
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_home(client):
+    """Ստուգում է գլխավոր էջի պատասխանը։"""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, CI/CD World!" in response.data
